6936b01358ae830f6ba145594aef63f0cadb6061
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-06-19 14:48:00 +0200

    Un case pour les cocher toutes

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,3 +7,9 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+
+
+// https://github.com/rbague/stimulus-check-all
+
+import CheckAll from "stimulus-check-all"
+application.register("check-all", CheckAll)
