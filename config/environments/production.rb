0faa0d0f4770028a51bdbeed6412cdf820ccbaec
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-07-15 08:57:11 +0200

    Changement domain SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'planning4-demo.heroku.com',
+    :domain         => 'mg.philnoug.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
