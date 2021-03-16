11abe98e521b347e88499210b268abc106419144
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-03-16 11:00:15 +0100

    Ajout de lacaud.iae aux membres RH

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,8 +25,9 @@ class User < ApplicationRecord
         'fitsch-mouras.iae@univ-paris1.fr',
         'manzano.iae@univ-paris1.fr',
         'denis.iae@univ-paris1.fr',
-        'riviere.iae@univ-paris1.fr']
-        .include?(self.email)
+        'riviere.iae@univ-paris1.fr',
+        'lacaud.iae@univ-paris1.fr'
+      ].include?(self.email)
   end
 
   def nom_et_prénom
