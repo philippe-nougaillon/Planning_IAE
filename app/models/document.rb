725dd4843199746977a64cc8938e9a91a9538fe3
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-03-29 13:26:51 +0200

    Workflow dossier & document

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,16 @@
 class Document < ApplicationRecord
+  include Workflow
+  include WorkflowActiverecord
+
+  audited
+
   belongs_to :dossier
+  has_one_attached :fichier
+
+  workflow do
+    state :nouveau
+    state :validé
+    state :refusé
+  end
+
 end
--- app/models/dossier.rb
+++ app/models/dossier.rb
@@ -1,3 +1,19 @@
 class Dossier < ApplicationRecord
+  include Workflow
+  include WorkflowActiverecord
+
+  audited
+
   belongs_to :intervenant
+  has_many :documents
+
+  workflow do
+    state :nouveau
+    state :envoyé
+    state :crée
+    state :validé
+    state :refusé
+    state :archivé
+  end
+
 end
