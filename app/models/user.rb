8815709d91d5e8b5281375eccad7b8bc4457b806
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-04-07 16:50:40 +0200

    Dossiers dans la nav

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,9 +23,7 @@ class User < ApplicationRecord
       ['philippe.nougaillon@gmail.com',
         'cunha.iae@univ-paris1.fr',
         'fitsch-mouras.iae@univ-paris1.fr',
-        'manzano.iae@univ-paris1.fr',
-        'denis.iae@univ-paris1.fr',
-        'riviere.iae@univ-paris1.fr'
+        'denis.iae@univ-paris1.fr'
       ].include?(self.email)
   end
 
