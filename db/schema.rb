c7a1e0bff9adf3d3dc7aec9105ff6887bd1b77ee
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-04-17 09:20:48 +0200

    schema load

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(version: 2020_09_03_132046) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "audits", id: :serial, force: :cascade do |t|
@@ -102,6 +108,18 @@ ActiveRecord::Schema.define(version: 2020_09_03_132046) do
     t.index ["formation_id"], name: "index_documents_on_formation_id"
     t.index ["intervenant_id"], name: "index_documents_on_intervenant_id"
     t.index ["unite_id"], name: "index_documents_on_unite_id"
+  end
+
+  create_table "dossiers", force: :cascade do |t|
+    t.bigint "intervenant_id", null: false
+    t.string "période"
+    t.string "workflow_state"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.string "mémo"
+    t.index ["intervenant_id"], name: "index_dossiers_on_intervenant_id"
+    t.index ["slug"], name: "index_dossiers_on_slug"
   end
 
   create_table "envoi_logs", force: :cascade do |t|
