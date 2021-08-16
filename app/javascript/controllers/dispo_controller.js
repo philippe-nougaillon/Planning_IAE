8792f71ca8e2cfee0bd70c7cc0123ba8b4d8ac39
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-08-16 15:18:42 +0200

    #103 nouveau cours sans salle dispo FIXED

--- app/javascript/controllers/dispo_controller.js
+++ app/javascript/controllers/dispo_controller.js
@@ -3,66 +3,51 @@ import Rails from "@rails/ujs";
 import $ from 'jquery';
 
 export default class extends Controller {
-    static targets = [ 'id', 'date', 'duree', 'formation_id', 'intervenant_id', 'salles' ]
+  static targets = [ 'id', 'date', 'duree', 'formation_id', 'intervenant_id', 'salles' ]
 
-    initialize() {
-      //this.clearResult()
-    }
+  connect() {
+    $("#intervenant_id").on('select2:select', function () {
+        //console.log("list item selected");
+        let event = new Event('change', { bubbles: true }) // fire a native event
+        this.dispatchEvent(event);
+    });
+  }
 
-    connect() {
-      //console.log("Hello, Stimulus!", this.element)
-
-      $("#intervenant_id").on('select2:select', function () {
-          //console.log("list item selected");
-          let event = new Event('change', { bubbles: true }) // fire a native event
-          this.dispatchEvent(event);
-      });
-    }
-
-    show_dispo() {
-      //console.log("Show_dispo!!")
-      this.updateAvailableRooms()
-    }
-    
-    toggleLoading() {
-      this.targets.find("button").classList.toggle("is-loading")
-    }
-    
-    updateAvailableRooms() {
-      //this.clearResult()
+  show_dispo() {
+    this.updateAvailableRooms()
+  }
   
-      Rails.ajax({
-        type: "GET",
-        url:  "/salles/libres.json",
-        data: "date=" + this.dateTarget.value
-              + "&id=" + this.idTarget.value 
-              + "&duree=" + this.dureeTarget.value
-              + "&formation_id=" + this.formation_idTarget.value
-              + "&intervenant_id=" + this.intervenant_idTarget.value,
-        success: (data) => {
-          //console.log('Available rooms loaded!')
-          this.refreshDropdownValues(data)
-        }
-      })
-    }
-    
-    refreshDropdownValues(data) {
-      this.sallesTarget.innerHTML = ""
-
-      if (data.length > 0) {
-        for(var i = 0; i < data.length; i++) {
-          var opt = data[i]
-          this.sallesTarget.innerHTML += "<option value=\"" + opt.id + "\">" + opt.nom + " (" + opt.places +"p)" + "</option>"
-        }
-      } else {
-        this.sallesTarget.innerHTML += "<option>Salle ou intervenant pas disponible</option>"
+  toggleLoading() {
+    this.targets.find("button").classList.toggle("is-loading")
+  }
+  
+  updateAvailableRooms() {
+    Rails.ajax({
+      type: "GET",
+      url:  "/salles/libres.json",
+      data: "date=" + this.dateTarget.value
+            + "&id=" + this.idTarget.value 
+            + "&duree=" + this.dureeTarget.value
+            + "&formation_id=" + this.formation_idTarget.value
+            + "&intervenant_id=" + this.intervenant_idTarget.value,
+      success: (data) => {
+        this.refreshDropdownValues(data)
       }
-    }
+    })
+  }
   
-    clearResult() {
-      //this.sallesTarget.innerHTML = ""
-    }
+  refreshDropdownValues(data) {
+    this.sallesTarget.innerHTML = ""
 
+    if (data.length > 0) {
+      for(var i = 0; i < data.length; i++) {
+        var opt = data[i]
+        this.sallesTarget.innerHTML += "<option value=\"" + opt.id + "\">" + opt.nom + " (" + opt.places +"p)" + "</option>"
+      }
+    } else {
+      this.sallesTarget.innerHTML += "<option disabled>Oups ! Aucune salle disponible... Vérifiez vos paramètres. </option>"
+    }
+  }
 }
 
 // https://gorails.com/forum/populate-dropdowns-based-on-selection-with-stimulus-js
