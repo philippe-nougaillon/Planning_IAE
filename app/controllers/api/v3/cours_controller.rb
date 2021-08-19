4ebe8fa24e08d7664479e36b8c47f523829673fc
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-08-19 14:20:08 +0200

    ne pas paginer si filtre

--- app/controllers/api/v3/cours_controller.rb
+++ app/controllers/api/v3/cours_controller.rb
@@ -1,21 +1,20 @@
 module Api
   	module V3
 		class Api::V3::CoursController < ActionController::Base
-
 			# Prevent CSRF attacks by raising an exception.
 			# For APIs, you may want to use :null_session instead.
 			protect_from_forgery with: :exception
-
 			skip_before_action :verify_authenticity_token
 			
 			def index
-				@cours = Cour.where("DATE(debut) >= ?", params[:d])
+				@cours = Cour.where("DATE(debut) >= ?", params[:d]).order(:debut, :fin)
 				unless params[:search].blank?
 					s = params[:search].upcase
-					@cours = @cours.joins(:formation, :intervenant).where("formations.nom LIKE ? OR intervenants.nom LIKE ?", "%#{ s }%", "%#{ s }%")					
+					@cours = @cours.joins(:formation, :intervenant)
+									.where("formations.nom LIKE ? OR intervenants.nom LIKE ?", "%#{ s }%", "%#{ s }%")					
+				else
+					@cours = @cours.paginate(page: params[:page], per_page: 30)
 				end
-				@cours = @cours.order(:debut, :fin)
-				@cours = @cours.paginate(page: params[:page], per_page: 25)
 			end
 			
 		end
