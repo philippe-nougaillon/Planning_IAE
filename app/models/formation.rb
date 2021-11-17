b8187f7c2192cbd4297c935a0f4d8c89a9543868
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-11-17 11:46:56 +0100

    suppression formation Fix

--- app/models/formation.rb
+++ app/models/formation.rb
@@ -5,16 +5,12 @@ class Formation < ApplicationRecord
 	has_many :users
 	has_many :cours, dependent: :destroy
 	has_many :intervenants, through: :cours
-	has_many :documents, dependent: :destroy
-	
 	has_many :unites
 	accepts_nested_attributes_for :unites, allow_destroy:true, 
 									reject_if: lambda {|attributes| attributes['num'].blank?}
-
 	has_many :etudiants
 	accepts_nested_attributes_for :etudiants, allow_destroy:true, 
 									reject_if: lambda {|attributes| attributes['nom'].blank?}
-
 	has_many :vacations
 	accepts_nested_attributes_for :vacations, allow_destroy:true, 
 									reject_if: lambda {|attributes| attributes['intervenant_id'].blank? || 
