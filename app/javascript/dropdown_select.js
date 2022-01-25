dbc02fff5fbfb1e81307043fcafa9389e7f0cbe6
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-01-25 12:46:57 +0100

    select2 class name changed

--- app/javascript/dropdown_select.js
+++ app/javascript/dropdown_select.js
@@ -3,6 +3,6 @@ import 'select2/dist/css/select2.css'
 import 'select2'
 
 $(document).on('turbolinks:load', function(){ 
-  $('.select2').select2({width: "100%"})
-  $('.select2-sm').select2()
+  $('.select2').select2()
+  $('.select2-w100').select2({width: "100%"})
 })
