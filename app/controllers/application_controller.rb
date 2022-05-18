32bf5065732424c156160d67e1df9b83d00a2520
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2022-05-18 10:14:06 +0200

    v4.8.f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
       @ctrl = params[:controller]
       @action = params[:action]
       @sitename ||= request.subdomains.any? ? request.subdomains(0).first.upcase : 'IAE-Planning DEV'
-      @sitename.concat(' v4.8.e')
+      @sitename.concat(' v4.8.f')
 
       if current_user
         @cours_params = {}
