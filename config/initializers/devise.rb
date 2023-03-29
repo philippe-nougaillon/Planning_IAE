b955aaaa0afdcb32cb0779729f1c9e1378b8a2ff
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-03-29 09:28:57 +0200

    devise mailer_sender changed

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'philippe.nougaillon@gmail.com'
+  config.mailer_sender = 'planning-iae@philnoug.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
