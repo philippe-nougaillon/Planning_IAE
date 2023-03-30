2f2699f5d1a6a8b06b25012d65ee386cbacb572d
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-03-30 14:12:43 +0200

    v4.16

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
       @ctrl = params[:controller]
       @action = params[:action]
       @sitename ||= request.subdomains.any? ? request.subdomains(0).first.upcase : 'IAE-Planning DEV'
-      @sitename.concat(' v4.15.d')
+      @sitename.concat(' v4.16')
 
       if current_user
         @cours_params = {}
