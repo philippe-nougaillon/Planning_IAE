764062715f43e8c40edcde1476a78e50d7aa281d
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-06-21 14:47:06 +0200

    4.19 : salle discarded fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
       @ctrl = params[:controller]
       @action = params[:action]
       @sitename ||= request.subdomains.any? ? request.subdomains(0).first.upcase : 'IAE-Planning DEV'
-      @sitename.concat(' v4.18.c')
+      @sitename.concat(' v4.19')
 
       if current_user
         @cours_params = {}
