e4fa8fbcd9f17b2579809e86cfbf3c4a211a7497
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-01-15 14:49:17 +0100

    Pundit depreciation fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   
-  include Pundit
+  include Pundit::Authorization
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
     
   # Prevent CSRF attacks by raising an exception.
