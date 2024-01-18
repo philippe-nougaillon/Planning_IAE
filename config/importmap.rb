0accc880a692de9569acc8a03559bf618ed1cdea
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-01-18 15:45:03 +0100

    rails7 : action_controller reworking

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,14 +2,11 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
-pin "popper.js", to: "https://ga.jspm.io/npm:popper.js@1.16.1/dist/umd/popper.js"
-pin "select2", to: "https://ga.jspm.io/npm:select2@4.1.0-rc.0/dist/js/select2.js"
-pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
-pin "stimulus", to: "https://ga.jspm.io/npm:stimulus@2.0.0/dist/stimulus.umd.js"
+pin "stimulus", to: "https://ga.jspm.io/npm:stimulus@3.2.2/dist/stimulus.js"
 pin "stimulus-check-all", to: "https://ga.jspm.io/npm:stimulus-check-all@0.6.0/src/index.js"
 pin "@github/check-all", to: "https://ga.jspm.io/npm:@github/check-all@0.4.0/dist/index.js"
+pin "trix", to: "https://ga.jspm.io/npm:trix@2.0.8/dist/trix.esm.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
