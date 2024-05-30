4d41fcfae6b40f39e57d05f5b6f382a40bb6e919
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-05-30 11:59:45 +0200

    commandes : devs removed from mails

--- app/mailers/tools_mailer.rb
+++ app/mailers/tools_mailer.rb
@@ -2,7 +2,7 @@ class ToolsMailer < ApplicationMailer
 
   def nouvelle_commande
     @cour = params[:cour]
-    mail(to: "logistique@iae.pantheonsorbonne.fr", cc: "philippe.nougaillon@gmail.com, pierreemmanuel.dacquet@gmail.com", 
+    mail(to: "logistique@iae.pantheonsorbonne.fr", 
          subject:"[PLANNING] Nouvelle commande pour le #{l @cour.debut, format: :long}").tap do |message|
           message.mailgun_options = {
             "tag" => ["logistique@iae.pantheonsorbonne.fr", "nouvelle_commande"]
@@ -13,7 +13,7 @@ class ToolsMailer < ApplicationMailer
   def commande_modifiée
     @cour = params[:cour]
     @old_commentaires = params[:old_commentaires]
-    mail(to: "logistique@iae.pantheonsorbonne.fr", cc: "philippe.nougaillon@gmail.com, pierreemmanuel.dacquet@gmail.com", 
+    mail(to: "logistique@iae.pantheonsorbonne.fr", 
       subject:"[PLANNING] Commande modifiée pour le #{l @cour.debut, format: :long}").tap do |message|
       message.mailgun_options = {
         "tag" => ["logistique@iae.pantheonsorbonne.fr", "commande_modifiée"]
@@ -24,7 +24,7 @@ class ToolsMailer < ApplicationMailer
   def commande_supprimée
     @cour = params[:cour]
     @old_commentaires = params[:old_commentaires]
-    mail(to: "logistique@iae.pantheonsorbonne.fr", cc: "philippe.nougaillon@gmail.com, pierreemmanuel.dacquet@gmail.com", 
+    mail(to: "logistique@iae.pantheonsorbonne.fr", 
       subject:"[PLANNING] Commande supprimée pour le #{l @cour.debut, format: :long}").tap do |message|
       message.mailgun_options = {
         "tag" => ["logistique@iae.pantheonsorbonne.fr", "commande_supprimée"]
@@ -34,7 +34,7 @@ class ToolsMailer < ApplicationMailer
 
   def rappel_commandes
     @cours = params[:cours]
-    mail(to: "logistique@iae.pantheonsorbonne.fr", cc: "philippe.nougaillon@gmail.com, pierreemmanuel.dacquet@gmail.com", 
+    mail(to: "logistique@iae.pantheonsorbonne.fr", 
       subject:"[PLANNING] Rappel des commandes").tap do |message|
       message.mailgun_options = {
         "tag" => ["logistique@iae.pantheonsorbonne.fr", "rappel_commandes"]
