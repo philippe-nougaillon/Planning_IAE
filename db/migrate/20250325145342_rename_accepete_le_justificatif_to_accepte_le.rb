d932f81830c5775465962749d8550252bd894f17
Alexandre Meunier <alexaudmeunier@outlook.fr>
2025-03-26 11:10:13 +0100

    Justificatif : Renommage d'un champ 2

--- db/migrate/20250325145342_rename_accepete_le_justificatif_to_accepte_le.rb
+++ db/migrate/20250325145342_rename_accepete_le_justificatif_to_accepte_le.rb
@@ -1,5 +1,5 @@
 class RenameAccepeteLeJustificatifToAccepteLe < ActiveRecord::Migration[7.1]
   def change
-    rename_column :justificatifs, :accepte_le, :accepte_le
+    rename_column :justificatifs, :accepete_le, :accepte_le
   end
 end
