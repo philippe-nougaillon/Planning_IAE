b8f2e3bdc1aef612e88d9fab68d6ae4cd4e5a930
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2025-03-31 13:47:06 +0200

    v5.11 : email examen done

--- app/helpers/tools_helper.rb
+++ app/helpers/tools_helper.rb
@@ -57,7 +57,7 @@ module ToolsHelper
                 if audit.audited_changes['fin'].class.name == 'Array'
                     pretty_changes << "Horaire de fin modifié de '#{I18n.l(c.last.first, format: :long)}' à '#{I18n.l(c.last.last, format: :long)}'"
                 else
-                    pretty_changes << "Fin initialisée à '#{c.last}'"
+                    pretty_changes << "Fin initialisée à '#{I18n.l(c.last, format: :long)}'"
                 end
             when 'Etat'
                 if audit.audited_changes['etat'].class.name == 'Array'
