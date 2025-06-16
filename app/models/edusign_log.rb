f96fdfbb7ee085fdddfa6ac1121f2359948bb64b
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2025-06-16 14:50:31 +0200

    edusign_log : design / wording

--- app/models/edusign_log.rb
+++ app/models/edusign_log.rb
@@ -1,12 +1,31 @@
 class EdusignLog < ApplicationRecord
   belongs_to :user, optional: true
 
-  enum etat: {success: 0, 
+  enum etat: {succès: 0, 
               warning: 1, 
-              echec: 2, 
+              échec: 2, 
               }
 
   def username
     user&.username || "Serveur"
+  end
+
+  def icon_etat
+    if self.etat == 'succès'
+      'check-circle'
+    else
+      'times-circle'
+    end
+  end 
+  
+  def icon_color
+    case self.etat
+    when 'succès'
+      'text-success'
+    when 'échec', 
+      'text-error'
+    when 'warning'
+      'text-warning'
+    end
   end
 end
