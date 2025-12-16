7f6aec3e004c43612bfa072ed8fb80a0e0ad1d69
P-edacquet <p-edacquet@hotmail.fr>
2025-12-16 10:00:20 +0100

    Dossier : attachments for any year

--- app/mailers/dossier_mailer.rb
+++ app/mailers/dossier_mailer.rb
@@ -4,7 +4,7 @@ class DossierMailer < ApplicationMailer
     def dossier_email
         @dossier = params[:dossier]
 
-        attachments['Dossier de recrutement.pdf']   = File.read('app/assets/attachments/Dossier CEV 2025-2026.pdf')
+        attachments['Dossier de recrutement.pdf']   = File.read('app/assets/attachments/Dossier Recrutement CEV.pdf')
         attachments['Pièces à fournir.pdf']         = File.read('app/assets/attachments/Pièces à fournir_dossier de recrutement.pdf')
         attachments['Note réglementaire.pdf']       = File.read('app/assets/attachments/Note sur les conditions de recrutement.pdf')
         attachments['Etat prévisionnel.pdf']        = File.read('app/assets/attachments/Formulaire_V02-1.pdf')
