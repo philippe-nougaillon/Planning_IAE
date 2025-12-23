cec8b7edf73c2a637047a6fd05c116c6cae76ca6
P-edacquet <p-edacquet@hotmail.fr>
2025-12-23 10:32:02 +0100

    Uniformisation : env variable renamed

--- app/models/intervenant.rb
+++ app/models/intervenant.rb
@@ -127,7 +127,7 @@ class Intervenant < ApplicationRecord
   end
 
 	def self.examens_ids
-		ENV["ALL_INTERVENANT_EXAMENS_IDS"].split(',').map(&:to_i)
+		ENV["INTERVENANTS_EXAMENS"].split(',').map(&:to_i)
 	end
 
 	def self.sans_intervenant
