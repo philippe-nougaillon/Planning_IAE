64e8c0e1da864d824ac5ee271b7c5e47e2c0e74f
P-edacquet <p-edacquet@hotmail.fr>
2026-06-04 12:28:29 +0200

    #669 : Fonctionnement de la sauvegarde des vacations via les formations modifié

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   end
 
   resources :formations do
+    resources :vacations, only: [:create, :update, :destroy], module: :formations
     collection do
       post :action
       post :action_do
